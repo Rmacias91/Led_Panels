2aef7f19fb728bbeddcaf9cff5f55c158b90c6ba
Richard Macias <Rmacias2454@gmail.com>
2015-07-03 14:54:24 -0500

    Added the text cc file run and trying to exit

--- python_for_C.py
+++ python_for_C.py
@@ -2,9 +2,15 @@ import os
 
 
 def main():
-    os.system("ls")
+    os.system("./text-example -f fonts/5x8.bdf -r 16 -c2 -y8 -C255,0,0")
+    os.system("cntrl+D")
     
    
 
 
 main()
+
+
+
+#sudo ./text-example -f fonts/5x8.bdf -r 16 -c2 -y8 -C255,0,0
+# We want to change his code, Idk if we should add a CNTRL+D To exit out?
