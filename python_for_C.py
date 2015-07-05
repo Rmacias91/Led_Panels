11c494071d2c3d35f9c15b69ae684adcf97c28b9
Richard Macias <Rmacias2454@gmail.com>
2015-07-05 16:57:18 -0500

    New

--- python_for_C.py
+++ python_for_C.py
@@ -1,12 +1,20 @@
 import os
+from datetime import datetime
+import time
+
 
 
 def main():
-    os.system("sudo ~/display16x32/rpi-rgb-led-matrix/text-example -f ~/display16x32/rpi-rgb-led-matrix/fonts/5x8.bdf")
-   # os.system("cntrl+D")
-    
-   
 
+    #Call the function to start for the text LEDs
+    os.system("sudo ~/display16x32/rpi-rgb-led-matrix/text-example -f ~/display16x32/rpi-rgb-led-matrix/fonts/5x8.bdf")
+    
+    #Now display the time every .2 seconds
+    for I in range(0,9):
+        my_time = datetime.now().strftime("%A %b,%d %I:%M") 
+        time.sleep(10)  
+        os.system("%s" %my_time)
+    
 
 main()
 
