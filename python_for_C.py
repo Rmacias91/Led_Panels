9543ece46b1dd79b8ea053f779c83df1a9e7c665
Rmacias91 <Rmacias2454@gmail.com>
2015-07-14 19:08:53 -0700

    Weather and Time

--- python_for_C.py
+++ python_for_C.py
@@ -1,11 +1,11 @@
 import os
 from datetime import datetime
 import time
-
-'''
 import pyowm
 import re
+
 def weather():
+    echo = "echo"
     owm = pyowm.OWM('98adaec88275ac5e03a288efaee18a11')
     #Today's Weather
     observation = owm.weather_at_place('Chicago,il')
@@ -15,22 +15,37 @@ def weather():
     #Not implemented yet ^^""
 
 
-    print ("For today's weather in Chicago")
-    print("Looks like we have a "+ w.get_status())
+    Intro_today= "For today's weather in Chicago"
+    status_today = "Looks like we have a "+ w.get_status()
 
     clouds = str(w.get_clouds())
-    print("There's " + clouds + "% clouds in the sky")
+    clouds_today = "There's " + clouds + "% clouds in the sky"
 
     x= str(w.get_temperature('fahrenheit'))
 
     x = re.sub("[{},:']",'',x)
     x= re.sub("[t]",'T',x)
     x = re.sub("Temp_kf None",'',x)
-    print(x)
+    temp_today = x
+
+    Npp= (len())
+
+    for x in range (Npp):
+        os.system( echo + " " + Intro_today)
+        time.sleep(.1)
+    for x in range (Npp):
+        os.system( echo + " " + status_today)
+        time.sleep(.1)
+    for x in range (Npp):
+        os.system( echo + " " + clouds_today)
+        time.sleep(.1)
+    for x in range (Npp):
+        os.system( echo + " " + temp_today)
+        time.sleep(.1)
 
 
-weather()
-'''
+
+
 def main():
     echo = "echo"
 
@@ -39,27 +54,24 @@ def main():
 
     #Now display the time every .2 seconds
     startup = 0;
-    Intro = "Just Do It."
+    Intro = "Good Freaking Morning Richie and Roger"
 
     while True:
-
         Npp = (len(Intro)*10)
-	if(startup == 0):
-        	for y in range (Npp):
-			
-			time.sleep(.1)
-			os.system(echo + " " + Intro)
-	startup = 1;       
-	my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
-	Npp = (len(my_time)* 10)
-        
-	for x in range (Npp):
-		my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
-		time.sleep(.1)
-     		os.system(echo + " " + my_time)
-		
-	
-	
+
+	#if startup == 0:
+        for y in range (Npp):
+            time.sleep(.1)
+            os.system(echo + " " + Intro)
+        startup = 1
+        my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
+        Npp = (len(my_time)* 10)
+
+        for x in range (Npp):
+            my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
+            time.sleep(.1)
+            os.system(echo + " " + my_time)
+        weather()
 main()
 
 
