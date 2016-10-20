53f28a9efc49e0aff6696cfb05cc07741f8aa99e
Rmacias91 <Rmacias2454@gmail.com>
2016-10-20 12:00:42 -0500

    Most Updated before adding Timer

--- python_for_C.py
+++ python_for_C.py
@@ -1,11 +1,12 @@
 import os
 from datetime import datetime
 import time
-
-'''
 import pyowm
 import re
+import json
+
 def weather():
+    echo = "echo"
     owm = pyowm.OWM('98adaec88275ac5e03a288efaee18a11')
     #Today's Weather
     observation = owm.weather_at_place('Chicago,il')
@@ -15,51 +16,89 @@ def weather():
     #Not implemented yet ^^""
 
 
-    print ("For today's weather in Chicago")
-    print("Looks like we have a "+ w.get_status())
+    Intro_today= "For todays weather in Chicago"
+    status_today = "Looks like we have a "+ w.get_status()
 
     clouds = str(w.get_clouds())
-    print("There's " + clouds + "% clouds in the sky")
+    clouds_today = "Theres " + clouds + "% clouds in the sky"
 
     x= str(w.get_temperature('fahrenheit'))
 
     x = re.sub("[{},:']",'',x)
     x= re.sub("[t]",'T',x)
     x = re.sub("Temp_kf None",'',x)
-    print(x)
+    temp_today = x
+
+   # Npp= (len(Intro_today))
+
+    #for x in range (Npp):
+     #   os.system( echo + " " + Intro_today)
+      #  time.sleep(.1)
+
+   # Npp= (len(status_today))
+   # for x in range (Npp):
+    #    os.system( echo + " " + status_today)
+    #    time.sleep(.1)
+
+   # Npp= (len(clouds_today))
+   # for x in range (Npp):
+    #    os.system( echo + " " + clouds_today)
+     #   time.sleep(.1)
+    Npp= (len(temp_today))
+    for x in range (Npp):
+        os.system( echo + " " + temp_today)
+        time.sleep(.1)
+
+def twitter():
+    echo = "echo"
+    tweets_data_path = 'twitter_data.txt'
+    tweets_data = []
+    tweets_file = open(tweets_data_path, 'r')
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_data.append(tweet)
+        except:
+            continue
+
+    for tweeter in tweets_data:
+        Npp = (len(tweeter['text'])*10)
+        for y in range (Npp):
+            time.sleep(.1)
+	    try:
+	        #print(tweeter['text'])
+                os.system(echo + " " + tweeter["text"])
+            except:
+	        continue	 
 
 
-weather()
-'''
 def main():
     echo = "echo"
 
     #Call the function to start for the text LEDs
-   # os.system("sudo ~/display16x32/rpi-rgb-led-matrix/text-example -f ~/display16x32/rpi-rgb-led-matrix/fonts/5x8.bdf")
+    # os.system("sudo ~/display16x32/rpi-rgb-led-matrix/text-example -f ~/display16x32/rpi-rgb-led-matrix/fonts/5x8.bdf")
 
     #Now display the time every .2 seconds
     startup = 0;
-    Intro = "Just Do It."
-
+    message = ["Welcome to Amped I","Richies Desk"]
     while True:
-
-        Npp = (len(Intro)*10)
-	if(startup == 0):
-        	for y in range (Npp):
-			
-			time.sleep(.1)
-			os.system(echo + " " + Intro)
-	startup = 1;       
-	my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
-	Npp = (len(my_time)* 10)
         
-	for x in range (Npp):
-		my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
-		time.sleep(.1)
-     		os.system(echo + " " + my_time)
-		
-	
-	
+	for i in range (len(message)):
+            Npp = (len(message[i])*10)
+	    #if startup == 0:
+            for y in range (Npp):
+                time.sleep(.1)
+                os.system(echo + " " + message[i])
+            startup = 1
+            my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
+            Npp = (len(my_time)* 10)
+
+            for x in range (Npp):
+                my_time = datetime.now().strftime("%A %b,%d %I:%M%p")
+                time.sleep(.1)
+                os.system(echo + " " + my_time)
+            weather()
+           # twitter()
 main()
 
 
